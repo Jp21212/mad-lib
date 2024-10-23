82b2e3871e2e5c19fb8ecb83694e838f433aa094
Jp21212 <Jamesp2814@tuckahoeschools.org>
2024-10-23 12:28:25 +0000

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+adjective1 = input("Type an adjective: ")
+noun1 = input("Type an noun: ")
+adjective2 = input("Type an adjective: ")
+noun2 = input("Type an noun: ")
+adjective3 = input("Type an adjective: ")
+noun3 = input("Type an noun: ")
+adjective4 = input("Type an adjective: ")
+noun4 = input("Type an noun: ")
+adjective5 = input("Type an adjective: ")
+noun5 = input("Type an noun: ")
+adjective6 = input("Type an adjective: ")
+noun6 = input("Type an noun: ")
+print(f"""Once upon a time, there was a {adjective1} {noun1} who did not like her (adjective2) (noun2). Her daughter always disobeyed the 
+      kings orders and never followed his strict rules """) The (ajective3) (noun3) would always try to stick up for her daughter, and she would always 
+(adjective4) (noun4). She finally thought that she was going to do soemthing about this. She had a very (adjective5) (noun5) to escape her (Ajdective6) (noun6) She will
+run away for ever and ever. 
